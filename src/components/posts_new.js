a1aef170e0f53b36b67dcfda419a53aaf7c3073e
windynik <nikhilprem@rocketmail.com>
2018-09-18 21:11:05 +0530

    Implements forms! and other stuff

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,72 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom';
+import {Field, reduxForm} from 'redux-form';
+
 
 class PostNew extends Component{
+    renderField(field){
+        return(
+            <div className='form-group'>
+            <label>{field.label}</label>
+                <input 
+                className='form-control'
+                type="text"
+                {...field.input}            //this has pregenerated es6 knowledge
+                />
+                {field.meta.errors}          
+            </div>
+        )                      //^ that is done by redux form , for error handling!
+    }
+    
     render(){
         return(
             <div>
-                Posts new!
+                <div className='text-xs-right'>
+                    <Link className='btn btn-primary' to='/'>
+                    Back to main
+                    </Link>
+                </div>
+            <form>
+                <Field
+                label='Title of the post!'
+                name="title"
+                component={this.renderField}
+                />
+                <Field
+                label='Categories'
+                name='categories'
+                component={this.renderField}
+                />
+                <Field
+                label='Post content'
+                name='content'
+                component={this.renderField}
+                />
+
+            </form>
             </div>
         )
     }
 }
 
-export default PostNew;
+function validate(values){
+    const errors={};
+
+    if(!values.title || values.title.length < 3){
+        errors.title="Input a title that is longer is than 3 letters!"
+    };
+    if(!values.categories){
+        errors.categories="Input some category!"
+    };
+    if(!values.content){
+        errors.content="Input some conent please!"
+    };
+
+
+
+    return errors;
+}
+export default reduxForm ({
+    validate,
+    form:'PostsNewForm'
+})(PostNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import {reducer as formReducer} from 'redux-form';  //renaming the reducer property. cause the word reducer is too .. genearal.
 import PostsReducer from './reducer_posts'
 
 
 const rootReducer = combineReducers({
-  posts:PostsReducer
+  posts:PostsReducer,
+  form:formReducer
 });
 
 export default rootReducer;
