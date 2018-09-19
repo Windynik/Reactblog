643ea5021f3441a6f3b65439039a1f0a759185d5
windynik <nikhilprem@rocketmail.com>
2018-09-19 20:43:39 +0530

    Submission is done! now to other features!  , also added styles :D

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST ='create_post';
 
 
-const root_url='http://reduxblog.herokuapp.com/api';
+const root_url='https://reduxblog.herokuapp.com/api';
 const API_KEY='?key=Keklords1234';
 
 export function fetchPosts(){
@@ -14,4 +15,12 @@ export function fetchPosts(){
         type:FETCH_POSTS,
         payload: request
     };
+}
+
+export function createPost(values){
+    const request=axios.post(`${root_url}/posts${API_KEY}`,values);
+    return{
+            type:CREATE_POST,
+            payload:request
+    }
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,33 +1,38 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {Field, reduxForm} from 'redux-form';
+import {connect} from 'react-redux';
+import {createPost} from '../actions';
 
 
 class PostNew extends Component{
+    
     renderField(field){
+        const {meta:{touched,error}} = field;               //destructuring! , taking the meta off of feild , making field.meta.error to meta.error , the "field" element is destructured.
+        const classNe=`form-group ${touched&&error?'has-danger' : ''}`
         return(
-            <div className='form-group'>
+            <div className={classNe}>
             <label>{field.label}</label>
                 <input 
                 className='form-control'
                 type="text"
                 {...field.input}            //this has pregenerated es6 knowledge
                 />
-                {field.meta.touched ? field.meta.error : ''}       
-                
-                }        
+                <div className="text-help"> 
+                {touched ? error : ''}        
+                </div>
             </div>
         )                      //^ that is done by redux form , for error handling!
                                 //the ternary operator is done because the if is not being rendered.
     }
     onSubmit(values){
-        console.log(values)
+        this.props.createPost(values);
     }
     render(){
         const {handleSubmit}=this.props;
         return(
             <div>
-                <div className='text-xs-right'>
+                <div className='text-xs-right kekek'>
                     <Link className='btn btn-primary' to='/'>
                     Back to main
                     </Link>
@@ -49,6 +54,7 @@ class PostNew extends Component{
                 component={this.renderField}
                 />
                 <button type="submit" className="btn btn-primary">Submit </button>
+                <Link className="btn btn-danger hek" to='/'>Cancel</Link>
             </form>
             </div>
         )
@@ -75,4 +81,7 @@ function validate(values){
 export default reduxForm ({
     validate,
     form:'PostsNewForm'
-})(PostNew);
+})(
+    connect(null,{createPost})(PostNew)                  //Parenthasis inside a parenthasis!!
+    
+);
